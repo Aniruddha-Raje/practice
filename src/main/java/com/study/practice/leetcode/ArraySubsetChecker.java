f46201a396cbc0618b60b6f2bdd85dc6668f288f
Aniruddha Raje <aniruddharaje@Aniruddhas-MacBook-Air.local>
2024-11-28 22:50:37 +0530

    added practice code

---
+++ src/main/java/com/study/practice/leetcode/ArraySubsetChecker.java
@@ -0,0 +1,34 @@
+package com.study.practice.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+public class ArraySubsetChecker {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {1, 2, 3, 4, 5};
+
+        boolean isSubset = isArraySubset(array1, array2);
+        log.info("Is array1 a subset of array2? " + isSubset);
+    }
+
+    public static boolean isArraySubset(int[] arr1, int[] arr2) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : arr2) {
+            set.add(num);
+        }
+
+        for (int num : arr1) {
+            if (!set.contains(num)) {
+                //If any element from arr1 is not found in the set, return false.
+                return false;
+            }
+        }
+
+        //If all elements are found, return true
+        return true;
+    }
+}
